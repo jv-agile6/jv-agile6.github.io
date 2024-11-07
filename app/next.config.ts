b8e344a234964c13b55ef5084c70c34be7a87a6c
Jerry Vrabel <jerry.vrabel@agile6.com>
2024-11-07 13:30:36 -0600

    Dir shuffles

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+    basePath: "/jv-agile6.github.io/app/",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
